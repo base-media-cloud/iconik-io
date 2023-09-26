1a9788fdf2be0a48d24f78a9333b0f11f585367a
Ross Maylor <ross.maylor@base-mc.com>
2023-09-26 15:49:15 +0100

    Fixed validation bug

--- pkg/iconikio/validate.go
+++ pkg/iconikio/validate.go
@@ -1,14 +1,14 @@
 package iconikio
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/google/uuid"
 	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
+
+	"github.com/google/uuid"
 )
 
 var validationRules = map[string][]string{
@@ -61,26 +61,7 @@ func IconikStatusCode(res *http.Response) error {
 }
 
 func (i *Iconik) validateFilename(index int) error {
-	// check filename exists in given collection id
-	var c *Collection
-	result, err := url.JoinPath(i.IconikClient.Config.APIConfig.Host, i.IconikClient.Config.APIConfig.Endpoints.Collection.Get.Path)
-	if err != nil {
-		return err
-	}
-	u, err := url.Parse(result)
-	if err != nil {
-		return err
-	}
-	u.Scheme = i.IconikClient.Config.APIConfig.Scheme
-	_, resBody, err := i.getResponseBody(i.IconikClient.Config.APIConfig.Endpoints.Collection.Get.Method, u.String(), nil)
-	if err != nil {
-		return err
-	}
-	err = json.Unmarshal(resBody, &c)
-	if err != nil {
-		return err
-	}
-	for _, object := range c.Objects {
+	for _, object := range i.IconikClient.Assets {
 		for _, file := range object.Files {
 			if file.OriginalName == i.IconikClient.Config.CSVMetadata[index].OriginalNameStruct.OriginalName {
 				i.IconikClient.Config.CSVMetadata[index].IDStruct.ID = object.ID
@@ -88,6 +69,7 @@ func (i *Iconik) validateFilename(index int) error {
 			}
 		}
 	}
+
 	return fmt.Errorf("file %s does not exist in given collection id", i.IconikClient.Config.CSVMetadata[index].OriginalNameStruct.OriginalName)
 }
 
