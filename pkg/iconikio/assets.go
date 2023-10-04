2ad0b5a0357448ca6f882686943daa1649524c22
Ross Maylor <ross.maylor@base-mc.com>
2023-10-04 11:07:05 +0100

    Refactor

--- pkg/iconikio/assets.go
+++ pkg/iconikio/assets.go
@@ -62,7 +62,7 @@ func (i *Iconik) GetCollection(collectionID string, pageNo int) error {
 		return fmt.Errorf(strings.Join(i.IconikClient.Collection.Errors, ", "))
 	}
 
-	if i.IconikClient.Collection.Pages > 1 && i.IconikClient.Collection.Pages > pageNo {
+	for n := i.IconikClient.Collection.Pages; n > pageNo; n++ {
 		i.GetCollection(collectionID, pageNo+1)
 	}
 
