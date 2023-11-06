21e8387953453116002ccf22b5898bcbac6d3629
Ross Maylor <ross.maylor@base-mc.com>
2023-11-06 17:35:14 +0000

    Refactor

--- pkg/iconikio/reader.go
+++ pkg/iconikio/reader.go
@@ -14,20 +14,16 @@ import (
 )
 
 func (i *Iconik) ReadCSVFile() ([][]string, error) {
-	// open the provided CSV file
 	csvFile, err := os.Open(i.IconikClient.Config.Input)
 	if err != nil {
 		return nil, errors.New("error opening CSV file")
 	}
 	defer csvFile.Close()
 
-	// create a new reader
 	csvReader := csv.NewReader(csvFile)
 
-	// read all the CSV data into a 2D slice we can work with
 	csvData, err := csvReader.ReadAll()
 	if err != nil {
-		// return errors.New("error reading CSV file")
 		return nil, err
 	}
 
@@ -35,7 +31,6 @@ func (i *Iconik) ReadCSVFile() ([][]string, error) {
 }
 
 func (i *Iconik) ReadExcelFile() ([][]string, error) {
-
 	excelFile, err := excelize.OpenFile(i.IconikClient.Config.Input)
 	if err != nil {
 		return nil, err
@@ -54,23 +49,19 @@ func (i *Iconik) ReadExcelFile() ([][]string, error) {
 
 // UpdateIconik reads a 2D slice, verifies it, and uploads the data to the Iconik API.
 func (i *Iconik) UpdateIconik(metadataFile [][]string) error {
-	// the first row of the 2D slice will be the header row
 	csvHeaders := metadataFile[0]
 
-	// we then validate that the schema for the header row is correct
 	if csvHeaders[0] != "id" || csvHeaders[1] != "original_name" || csvHeaders[2] != "title" {
 		fmt.Println(csvHeaders)
 		return errors.New("CSV file not properly formatted for Iconik")
 	}
 
-	// get the slimmed down 2D slice that contains only the columns matched to the given metadata view
 	matchingData, nonMatchingHeaders, err := i.matchCSVtoAPI(metadataFile)
 	if err != nil {
 		return err
 	}
 
 	if len(nonMatchingHeaders) > 0 {
-		// log to the user if there were any columns in the provided file that have been left out
 		fmt.Println("Some columns from the file provided have not been included in the upload to Iconik, as they are not part of the metadata view provided. Please see below for the headers of the columns not included:")
 		fmt.Println()
 		for _, nonMatchingHeader := range nonMatchingHeaders {
@@ -78,9 +69,7 @@ func (i *Iconik) UpdateIconik(metadataFile [][]string) error {
 		}
 	}
 
-	// get the file Header Names row
 	matchingFileHeaderNames := matchingData[0]
-	// get the file Header Labels row
 	matchingFileHeaderLabels := matchingData[1]
 
 	i.IconikClient.Config.CSVFilesToUpdate = len(matchingData) - 2
@@ -89,7 +78,6 @@ func (i *Iconik) UpdateIconik(metadataFile [][]string) error {
 	for index := 2; index < len(matchingData); index++ {
 		row := matchingData[index]
 
-		// Create a new instance of CSVMetadata.
 		csvMetadata := CSVMetadata{
 			Added: false,
 			IDStruct: IDStruct{
@@ -123,7 +111,6 @@ func (i *Iconik) UpdateIconik(metadataFile [][]string) error {
 			headerLabel := matchingFileHeaderLabels[count]
 			fieldValueSlice := make([]FieldValue, 0)
 
-			// if there are words separated by spaces inside the field, split with a comma
 			valueArr := strings.Split(row[count], ",")
 			if !isBlankStringArray(valueArr) {
 				for _, val := range valueArr {
@@ -135,7 +122,6 @@ func (i *Iconik) UpdateIconik(metadataFile [][]string) error {
 
 					fieldValueSlice = append(fieldValueSlice, FieldValue{Value: val})
 				}
-				// Add the fieldValueSlice to the specific headerName in MetadataValues
 				csvMetadata.MetadataValuesStruct.MetadataValues[headerName] = struct {
 					FieldValues []FieldValue `json:"field_values"`
 				}{
@@ -146,13 +132,11 @@ func (i *Iconik) UpdateIconik(metadataFile [][]string) error {
 			}
 		}
 
-		// update the title name for this particular asset/row
 		err = i.updateTitle(index - 2)
 		if err != nil {
 			return err
 		}
 
-		// update the remaining metadata for this particular asset/row
 		err = i.updateMetadata(index - 2)
 		if err != nil {
 			return err
@@ -166,7 +150,6 @@ func (i *Iconik) UpdateIconik(metadataFile [][]string) error {
 	for _, csvMetadata := range i.IconikClient.Config.CSVMetadata {
 		if csvMetadata.Added {
 			countSuccess++
-			// log.Printf("%s (Title: %s, Original filename: %s)", csvMetadata.IDStruct.ID, csvMetadata.TitleStruct.Title, csvMetadata.OriginalNameStruct.OriginalName)
 		}
 	}
 	fmt.Printf("%d of %d", countSuccess, i.IconikClient.Config.CSVFilesToUpdate)
@@ -185,9 +168,7 @@ func (i *Iconik) UpdateIconik(metadataFile [][]string) error {
 	return nil
 }
 
-// updateTitle updates the title for the given asset ID.
 func (i *Iconik) updateTitle(index int) error {
-
 	requestBody, err := json.Marshal(i.IconikClient.Config.CSVMetadata[index].TitleStruct)
 	if err != nil {
 		return errors.New("error marshaling JSON")
@@ -206,7 +187,6 @@ func (i *Iconik) updateTitle(index int) error {
 	res, resBody, err := i.getResponseBody(i.IconikClient.Config.APIConfig.Endpoints.Asset.Patch.Method, u.String(), bytes.NewBuffer(requestBody))
 
 	if res.StatusCode == 200 {
-		// log.Printf("Successfully updated title name for asset %s (%s)", i.IconikClient.Config.CSVMetadata[index].IDStruct.ID, i.IconikClient.Config.CSVMetadata[index].OriginalNameStruct.OriginalName)
 	} else {
 		log.Println("Error updating title name for asset ", i.IconikClient.Config.CSVMetadata[index].IDStruct.ID)
 		log.Println("resBody:", string(resBody))
@@ -217,9 +197,7 @@ func (i *Iconik) updateTitle(index int) error {
 	return nil
 }
 
-// updateMetadata updates the metadata for the given asset ID.
 func (i *Iconik) updateMetadata(index int) error {
-
 	requestBody, err := json.Marshal(i.IconikClient.Config.CSVMetadata[index].MetadataValuesStruct)
 	if err != nil {
 		return errors.New("error marshaling JSON")
